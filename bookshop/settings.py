10d2800b0150626351b022b216ddfdc89274e5fc
AlexandraH-code <alexandra_holstensson@hotmail.com>
2025-09-05 17:13:30 +0200

    Add templates and index.html in Home app

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p_qa3ob$#gp(g%(nk@)8bz3!=ocir_%iskiryy_aio=*_)jt+0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1',
                  '.herokuapp.com',]
